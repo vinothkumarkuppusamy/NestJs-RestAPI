8174b0ac8e13e64ea4d6fd2e57152da56bf56647
vinothkumar k <vinoth@eziosolutions.com>
2025-01-06 18:30:13 +0530

    custom validation pipe added

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { createAuthDto } from './dto/createAuth.dto';
+import { idparamsPipe } from './dto/idParams.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
     @Get()
-    signup(){
+    @HttpCode(202)
+    getUser(@Query("id") Param: idparamsPipe){
         return 'This is a signup endpoint';
+    }   
+    @Post()
+    //@UsePipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}))
+    //@HttpCode(201)
+    createUser(@Body() Body: createAuthDto){
+        return Body;
     }
+
 }
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,7 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
-
 @Module({
-  controllers: [AuthenticationController]
+  controllers: [AuthenticationController],
+  // Request validation for module level
+  providers:[{
+    provide: 'APP_PIPE',
+    useValue: new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
+    }) 
+  }
+  ],
 })
 export class AuthenticationModule {}
